4d76313873f7974487322b8c3dac41d55b0b7fa6
asuchonska <91882538+asuchonska@users.noreply.github.com>
2023-07-03 06:55:37 +0200

    Final version ready for build

--- Assets/Scripts/MenuLogic.cs
+++ Assets/Scripts/MenuLogic.cs
@@ -11,7 +11,7 @@ public class MenuLogic : MonoBehaviour
 
     public void PlayButton()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 
     public void Controls()
