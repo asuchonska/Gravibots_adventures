5609e62b2929b753a22062ac5dbee6c5a59ebee9
asuchonska <91882538+asuchonska@users.noreply.github.com>
2023-07-03 06:24:52 +0200

    final UI

---
+++ Assets/Scripts/MenuLogic.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuLogic : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject controlInfo;
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Controls()
+    {
+        mainMenu.gameObject.SetActive(false);
+        controlInfo.gameObject.SetActive(true);
+    }
+    
+    
+    public void ControlsBack()
+    {
+        controlInfo.gameObject.SetActive(false);
+        mainMenu.gameObject.SetActive(true);
+
+    }
+    
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+
+
+}
